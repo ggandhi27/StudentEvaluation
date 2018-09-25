fc4692c469b5670f8da3f507274169ac75056286
Gaurav Gandhi <grv.gandhi.27@gmail.com>
2018-09-25 19:33:41 +0530

    Repo updated.

---
+++ src/main/java/student/evaluation/StudentEvaluation/HelloWorld.java
@@ -0,0 +1,15 @@
+package student.evaluation.StudentEvaluation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorld {
+
+	@RequestMapping(value="/")
+	@ResponseBody
+	public String hello() {
+		return "Hello Shiwangi";
+	}
+}
