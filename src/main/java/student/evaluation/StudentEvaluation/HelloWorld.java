862a8720896869657f03aef47d0d53f60a415941
Gaurav Gandhi <grv.gandhi.27@gmail.com>
2018-09-26 00:54:26 +0530

    Repo updated

--- src/main/java/student/evaluation/StudentEvaluation/HelloWorld.java
+++ src/main/java/student/evaluation/StudentEvaluation/HelloWorld.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloWorld {
 
-	@RequestMapping(value="/",method = RequestMethod.GET)
+	@RequestMapping(value="/")
 	public String hello() {
 		return "Hello Bruh";
 	}
