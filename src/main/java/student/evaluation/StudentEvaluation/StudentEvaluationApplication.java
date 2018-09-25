96577f391429129c088979a3df1fb50e752702cd
Gaurav Gandhi <grv.gandhi.27@gmail.com>
2018-09-25 19:16:07 +0530

    Repository Initialized

---
+++ src/main/java/student/evaluation/StudentEvaluation/StudentEvaluationApplication.java
@@ -0,0 +1,12 @@
+package student.evaluation.StudentEvaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentEvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentEvaluationApplication.class, args);
+	}
+}
---
+++ src/test/java/student/evaluation/StudentEvaluation/StudentEvaluationApplicationTests.java
@@ -0,0 +1,16 @@
+package student.evaluation.StudentEvaluation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentEvaluationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
