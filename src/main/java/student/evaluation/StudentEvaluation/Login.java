0edc181c5c346f293172f26297d18c475806c937
Gaurav Gandhi <gauravgo4friends@gmail.com>
2018-11-12 12:16:32 +0530

    Minor changes done at the time of presentation.

--- src/main/java/student/evaluation/StudentEvaluation/Login.java
+++ src/main/java/student/evaluation/StudentEvaluation/Login.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Login {
 	
 	@RequestMapping(value="/")
-	public String login() {
+	public String Login() {
 		
 		return "login.html";
 		
