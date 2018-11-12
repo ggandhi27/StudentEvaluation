5d7813f6543bd2de91cdf989a2f7d0cfeca648c2
Gaurav Gandhi <gauravgo4friends@gmail.com>
2018-11-12 15:05:25 +0530

    Login service module is created. But it is not completed.

--- src/main/java/student/evaluation/StudentEvaluation/Login.java
+++ src/main/java/student/evaluation/StudentEvaluation/Login.java
@@ -1,17 +1,49 @@
 package student.evaluation.StudentEvaluation;
 
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import entity.User;
 
 @Controller
 public class Login {
 	
+	@Autowired
+	private LoginService loginService;
+	
+	
 	@RequestMapping(value="/")
-	public String Login() {
+	public String login() {
 		
 		return "login.html";
 		
 	}
 	
-
+	@RequestMapping(value="/Login")
+	@ResponseBody
+	public String checkLogin(@RequestParam(name="username")String username,
+			@RequestParam(name="password")String password) {
+		
+        
+		File file = new File("/tmp/"+username+".xml");		
+        
+		if (!file.exists()) {
+       	 return("User does not exists");
+       	}
+		else {
+			if(loginService.checkPassword(password)) {
+				User user = loginService.getUserObject(username);
+				
+			}
+			else {
+				return "Either username or password is incorrect.";
+			}
+		}
+		return "";
+	}
 }
---
+++ src/main/java/student/evaluation/StudentEvaluation/LoginService.java
@@ -0,0 +1,21 @@
+package student.evaluation.StudentEvaluation;
+
+import org.springframework.stereotype.Service;
+
+import entity.User;
+
+@Service
+public class LoginService {
+
+	public boolean checkPassword(String password) {
+		
+		return false;
+	}
+
+	public User getUserObject(String username) {
+		
+		
+		return null;
+	}
+	
+}
