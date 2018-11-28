863b17ec4cee6959d5f6d1dd0b482274e4474c84
Ankit Pathak <ankpathak595@gmail.com>
2018-11-28 18:11:47 +0530

    trying thymeleaf

--- src/main/java/student/evaluation/StudentEvaluation/Dashboard.java
+++ src/main/java/student/evaluation/StudentEvaluation/Dashboard.java
@@ -2,13 +2,17 @@ package student.evaluation.StudentEvaluation;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Dashboard {
 	
-	@RequestMapping(value="/dash")
-	public String Dash() {
-		return "dashboard.html";
+	@RequestMapping(value="/dash",method=RequestMethod.GET)
+	public ModelAndView Dash() {
+		ModelAndView model=new ModelAndView("dashboard.html");
+		model.addObject("username", "Ankit Pathak");
+		return model;
 	}
 }
