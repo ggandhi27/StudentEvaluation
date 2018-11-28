162b6d27f7176447062281a14ccfb636c6b2a781
Gaurav Gandhi <gauravgo4friends@gmail.com>
2018-11-28 18:41:12 +0530

    There is no issue with the thymeleaf. It is working fine. There is some problem with the css and the js files. Have to check it out

--- src/main/java/student/evaluation/StudentEvaluation/Dashboard.java
+++ src/main/java/student/evaluation/StudentEvaluation/Dashboard.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @Controller
 public class Dashboard {
