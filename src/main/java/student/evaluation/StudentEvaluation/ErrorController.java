ee3c019c3b9c81e4c6907ccd525d2a2002bdc7aa
Gaurav Gandhi <gauravgo4friends@gmail.com>
2018-11-30 20:08:30 +0530

    Testing error page redirection

--- src/main/java/student/evaluation/StudentEvaluation/ErrorController.java
+++ src/main/java/student/evaluation/StudentEvaluation/ErrorController.java
@@ -2,11 +2,12 @@ package student.evaluation.StudentEvaluation;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ErrorController {
 
-	@RequestMapping(value="/error")
+	@RequestMapping(value="/error",method=RequestMethod.GET)
 	public String errorMapping()
 	{
 		return "redirect:/";
