697ac1ba17831b8068048eee2ed2d21a831203d1
Gaurav Gandhi <gauravgo4friends@gmail.com>
2018-12-07 20:10:12 +0530

    Question entity added.

---
+++ src/main/java/entity/Question.java
@@ -0,0 +1,27 @@
+package entity;
+
+public class Question {
+
+	private String question;
+	private String choice[] = new String[4];
+	int answer;
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public String[] getChoice() {
+		return choice;
+	}
+	public void setChoice(String[] choice) {
+		this.choice = choice;
+	}
+	public int getAnswer() {
+		return answer;
+	}
+	public void setAnswer(int answer) {
+		this.answer = answer;
+	}
+	
+}
