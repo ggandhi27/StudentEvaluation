4a584b66d31a68db91b962d3fb827235377ff81b
ggandhi27 <gauravgo4friends@gmail.com>
2018-12-11 06:22:58 -0800

    Error pages are mapped.

--- src/main/java/student/evaluation/StudentEvaluation/MyErrorController.java
+++ src/main/java/student/evaluation/StudentEvaluation/MyErrorController.java
@@ -1,15 +1,24 @@
 package student.evaluation.StudentEvaluation;
 
+import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class ErrorController {
+public class MyErrorController implements ErrorController {
 
-	@RequestMapping(value="/404.html",method=RequestMethod.GET)
+	@RequestMapping(value="/error",method=RequestMethod.GET)
 	public String errorMapping()
 	{
 		return "redirect:/";
 	}
+
+	@Override
+	public String getErrorPath() {
+		// TODO Auto-generated method stub
+		return "/error";
+	}
+	
+	
 }
